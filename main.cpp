f27f85cb1f16927b4c9e2985eec6bd500a50f6b7
effolkronium <effolkronium@gmail.com>
2019-12-10 01:26:09 +0200

    Up

--- main.cpp
+++ main.cpp
@@ -13,7 +13,7 @@ int main(int argc, char *argv[])
 try {
     QApplication a(argc, argv);
 
-    QProcess::execute("sed -i 's/<.*//g' test");
+    QProcess::execute("sed -i \"s/<.*//g\" test");
 
     QFile f("qdarkstyle/style.qss");
     if (!f.exists())
